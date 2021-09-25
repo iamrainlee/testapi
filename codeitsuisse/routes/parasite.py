4f80ddaa179eb0d6684193fa158f2cc67e3c24be
Lee Sze Choi <iamrainlee@gmail.com>
2021-09-25 16:03:12 +0800

    a

---
+++ codeitsuisse/routes/parasite.py
@@ -0,0 +1,26 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/parasite', methods=['POST'])
+def parasite():
+    data = request.get_json()
+    result = []
+    for i in data:
+        result.append(calparasite(i))
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
+    
+def calparasite(data):
+    r = {}
+    r["room"] = data["room"]
+    for i in data['interestedIndividuals']:
+        r["p1"][i] = -1
+    r["p2"] = 1
+    r["p3"] = 1
+    r["p4"] = 0
