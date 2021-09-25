a3f2c72c3121b1f298beef858dd00b18669dc9d6
Lee Sze Choi <iamrainlee@gmail.com>
2021-09-25 16:42:08 +0800

    a

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -21,8 +21,53 @@ def calparasite(data):
     r["room"] = data["room"]
     r["p1"] = {}
     for i in data['interestedIndividuals']:
-        r["p1"][i] = -1
+        r["p1"][i] = calparasitep1(i,data["grid"])
     r["p2"] = 1
     r["p3"] = 1
     r["p4"] = 0
     return r
+def calparasitep1(ind, g):
+    grid = []
+    for i in g:
+        grid.append(i[:])
+    changed = True
+    ind = [int(i) for i in ind.split(',')]
+    tick = 0
+    if(grid[ind[0]][ind[1]] in [0,2,3]):
+        return -1
+    while changed:
+        tick += 1
+        changed = False
+        oldgrid = []
+        for i in grid:
+            oldgrid.append(i[:])
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if(oldgrid[i][j] == 3):
+                    try:
+                        if(grid[i+1][j] == 1):
+                            grid[i+1][j] = 3
+                            changed = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid[i-1][j] == 1):
+                            grid[i-1][j] = 3
+                            changed = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid[i][j+1] == 1):
+                            grid[i][j+1] = 3
+                            changed = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid[i][j-1] == 1):
+                            grid[i][j-1] = 3
+                            changed = True
+                    except:
+                        a = 1
+        if(grid[ind[0]][ind[1]] == 3):
+            return tick
+    return -1
