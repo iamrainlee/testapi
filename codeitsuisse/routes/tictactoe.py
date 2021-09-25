874c0403bf41d9c1c624883f2888f0610814b79b
Lee Sze Choi <iamrainlee@gmail.com>
2021-09-25 15:29:35 +0800

    a

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -141,3 +141,35 @@ def score(played,youAre):
         return -10
     else:
         return 0
+
+# def minimax(played,youAre):
+#     if youAre == 'O':
+#         opponent = 'X'
+#     else:
+#         opponent = 'O'
+#     if checkwin(played,youAre) or checkwin(played,opponent):
+#         return score(played,youAre)
+#     scores = [] # an array of scores
+#     moves = []  # an array of moves
+#
+#     # Populate the scores array, recursing as needed
+#     for i in range(len(played)):
+#         if(played[i] != ''):
+#             temp = played[:]
+#             temp[i] = youAre
+#             scores.append(minimax(temp[i],youAre))
+#             moves.append(i)
+#
+#     # Do the min or the max calculation
+#     if game.active_turn == @player
+#         # This is the max calculation
+#         max_score_index = scores.each_with_index.max[1]
+#         @choice = moves[max_score_index]
+#         return scores[max_score_index]
+#     else
+#         # This is the min calculation
+#         min_score_index = scores.each_with_index.min[1]
+#         @choice = moves[min_score_index]
+#         return scores[min_score_index]
+#     end
+# end
