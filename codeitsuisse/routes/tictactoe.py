ef3b81a07d5f46491e151d561da795e4c0355329
Lee Sze Choi <iamrainlee@gmail.com>
2021-09-25 11:12:48 +0800

    updates

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -10,25 +10,11 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/tic-tac-toe', methods=['POST'])
-
-def makemove(board,played,youAre,battleId):
-    data = {}
-    data['action'] = "putSymbol"
-    if(played.count('') == 9):
-        data["position"] = "NW"
-    else:
-        for i in range(len(played)):
-            if(played[i] == ''):
-                data["position"] = board[i]
-                break
-    requests.post(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/play/{battleId}',json.dumps(played))
-
-
 def tictactoe():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     battleId = data.get("battleId")
-    messages = SSEClient(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/start/{battleId}')
+    messages = SSEClient('https://cis2021-arena.herokuapp.com/tic-tac-toe/start/'+battleId)
     board = ['NW','N','NE','W','C','E','SW','S','SE']
     played = ['','','','','','','','','']
     youAre = ""
@@ -44,10 +30,22 @@ def tictactoe():
                 if(data['player'] == youAre):
                     continue
                 else:
-                    if(data["action"] = "putSymbol"):
+                    if(data["action"] == "putSymbol"):
                         played[board.index(data['position'])] = data['player']
                         makemove(board,played,youAre,battleId)
             except:
                 continue
     logging.info("My result :{}".format(result))
     return json.dumps(result)
+
+def makemove(board,played,youAre,battleId):
+    data = {}
+    data['action'] = "putSymbol"
+    if(played.count('') == 9):
+        data["position"] = "NW"
+    else:
+        for i in range(len(played)):
+            if(played[i] == ''):
+                data["position"] = board[i]
+                break
+    requests.post("https://cis2021-arena.herokuapp.com/tic-tac-toe/play/"+battleId, data = json.dumps(data))
