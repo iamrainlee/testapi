af462182aa34114b64f3fe26231e8e8fd2bc88f7
Lee Sze Choi <iamrainlee@gmail.com>
2021-09-25 17:34:16 +0800

    a

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -25,41 +25,98 @@ def calparasite(data):
     grid = []
     for i in data['grid']:
         grid.append(i[:])
+    grid2 = []
+    for i in data['grid']:
+        grid2.append(i[:])
     changed = True
     tick = 0
+    tick2 = 0
     while changed:
-        changed = False
+        changed1 = False
+        changed2 = False
         oldgrid = []
         for i in grid:
             oldgrid.append(i[:])
+        oldgrid2 = []
+        for i in grid2:
+            oldgrid2.append(i[:])
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if(oldgrid[i][j] == 3):
                     try:
                         if(grid[i+1][j] == 1):
                             grid[i+1][j] = 3
-                            changed = True
+                            changed1 = True
                     except:
                         a = 1
                     try:
                         if(grid[i-1][j] == 1):
                             grid[i-1][j] = 3
-                            changed = True
+                            changed1 = True
                     except:
                         a = 1
                     try:
                         if(grid[i][j+1] == 1):
                             grid[i][j+1] = 3
-                            changed = True
+                            changed1 = True
                     except:
                         a = 1
                     try:
                         if(grid[i][j-1] == 1):
                             grid[i][j-1] = 3
-                            changed = True
+                            changed1 = True
                     except:
                         a = 1
-        if(changed):
+                if(oldgrid2[i][j] == 3):
+                    try:
+                        if(grid2[i+1][j] == 1):
+                            grid2[i+1][j] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid2[i-1][j] == 1):
+                            grid2[i-1][j] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid2[i][j+1] == 1):
+                            grid2[i][j+1] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid2[i][j-1] == 1):
+                            grid2[i][j-1] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid2[i-1][j-1] == 1):
+                            grid2[i-1][j-1] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid2[i+1][j-1] == 1):
+                            grid2[i+1][j-1] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid2[i-1][j+1] == 1):
+                            grid2[i-1][j+1] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+                    try:
+                        if(grid2[i+1][j+1] == 1):
+                            grid2[i+1][j+1] = 3
+                            changed2 = True
+                    except:
+                        a = 1
+        if(changed1):
             tick += 1
             for p1 in r["p1"]:
                 ind = [int(i) for i in p1.split(',')]
@@ -68,86 +125,26 @@ def calparasite(data):
                 elif(r["p1"][p1] == -1):
                     if(grid[ind[0]][ind[1]] == 3):
                         r["p1"][p1] = tick
-    uninfected = False
+        if(changed2):
+            tick2 += 1
+        changed = changed1 or changed2
+    uninfected1 = False
+    uninfected2 = False
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if(grid[i][j] in [1,2]):
                 uninfected = True
-                break
-    if(uninfected):
+            if(grid2[i][j] in [1,2]):
+                uninfected2 = True
+        if(uninfected1 and uninfected2):
+            break
+    if(uninfected1):
         r["p2"] = -1
     else:
         r["p2"] = tick
-    r["p3"] = calculatep3(data['grid'])
+    if(uninfected2):
+        r["p3"] = -1
+    else:
+        r["p3"] = tick2
     r["p4"] = 0
     return r
-
-def calculatep3(g):
-    grid = []
-    for i in g:
-        grid.append(i[:])
-    changed = True
-    tick = 0
-    while changed:
-        changed = False
-        oldgrid = []
-        for i in grid:
-            oldgrid.append(i[:])
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if(oldgrid[i][j] == 3):
-                    try:
-                        if(grid[i+1][j] == 1):
-                            grid[i+1][j] = 3
-                            changed = True
-                    except:
-                        a = 1
-                    try:
-                        if(grid[i-1][j] == 1):
-                            grid[i-1][j] = 3
-                            changed = True
-                    except:
-                        a = 1
-                    try:
-                        if(grid[i][j+1] == 1):
-                            grid[i][j+1] = 3
-                            changed = True
-                    except:
-                        a = 1
-                    try:
-                        if(grid[i][j-1] == 1):
-                            grid[i][j-1] = 3
-                            changed = True
-                    except:
-                        a = 1
-                    try:
-                        if(grid[i-1][j-1] == 1):
-                            grid[i][j-1] = 3
-                            changed = True
-                    except:
-                        a = 1
-                    try:
-                        if(grid[i+1][j-1] == 1):
-                            grid[i][j-1] = 3
-                            changed = True
-                    except:
-                        a = 1
-                    try:
-                        if(grid[i-1][j+1] == 1):
-                            grid[i][j-1] = 3
-                            changed = True
-                    except:
-                        a = 1
-                    try:
-                        if(grid[i+1][j+1] == 1):
-                            grid[i][j-1] = 3
-                            changed = True
-                    except:
-                        a = 1
-        if(changed):
-            tick += 1
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if(grid[i][j] in [1,2]):
-                return -1
-    return tick
