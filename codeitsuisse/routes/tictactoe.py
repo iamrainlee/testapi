3e065bbb89157060c0bf0fd439e0bfcc419537d9
Lee Sze Choi <iamrainlee@gmail.com>
2021-09-25 13:08:40 +0800

    use sse

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -1,8 +1,6 @@
 import logging
 import json
-import asyncio
-import aiohttp
-from aiosseclient import aiosseclient
+from sseclient import SSEClient
 import requests
 
 from flask import request, jsonify
@@ -21,9 +19,10 @@ def tictactoe():
     youAre = ""
     gameOn = True
     while gameOn :
-        for event in aiosseclient('https://cis2021-arena.herokuapp.com/tic-tac-toe/start/'+battleId):
+        messages = SSEClient('https://cis2021-arena.herokuapp.com/tic-tac-toe/start/'+battleId)
+        for msg in messages:
             # r = requests.get('https://cis2021-arena.herokuapp.com/tic-tac-toe/start/'+battleId)
-            data = event
+            data = msg.data
             logging.info("data sent from arena {}".format(data))
             # data = json.loads(msg.data.replace("'",'"'))
             if type(data) is str:
