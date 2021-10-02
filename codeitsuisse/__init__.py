14eb16ca8687ec1561e83881959bcc8a548b87b0
Lee Sze Choi <iamrainlee@gmail.com>
2021-10-02 14:49:50 +0800

    a

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,11 +1,3 @@
 from flask import Flask
 app = Flask(__name__)
-import codeitsuisse.routes.square
-import codeitsuisse.routes.tictactoe
-import codeitsuisse.routes.parasite
-import codeitsuisse.routes.asteroid
-import codeitsuisse.routes.quoridor
-import codeitsuisse.routes.stockhunter
-import codeitsuisse.routes.stonks
-import codeitsuisse.routes.ciphercracking
-import codeitsuisse.routes.decoder
+import codeitsuisse.routes.testget
---
+++ codeitsuisse/routes/testget.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import logging
+import json
+from sseclient import SSEClient
+import requests
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/tic-tac-toe', methods=['GET'])
+def testget():
+    data = request.args
+    rdata = {}
+    if(data["action"] == "get_students"):
+        rdata = json.loads('[{"Name":"Nicole","UID":"3035785611","DOB":"2002-11-08","Local":"TRUE","Beautiful Index":1000},{"Name":"Rain","UID":"3035779571","DOB":"2002-04-05","Local":"TRUE","Beautiful Index":"NULL"}]')
+    else:
+        rdata["error"] = "Missing action"
+    return json.dumps(rdata)
