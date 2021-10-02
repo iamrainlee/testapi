ed69156c46420b396a6f6f7c512aef2f3f55acc7
Lee Sze Choi <iamrainlee@gmail.com>
2021-10-02 16:27:27 +0800

    a

--- codeitsuisse/routes/testpost.py
+++ codeitsuisse/routes/testpost.py
@@ -25,10 +25,12 @@ def testpost():
             rdata = json.loads('[{"id":1,"Name":"Merry","Program":"BBA","Local":"TRUE","Position":2,"Beautiful Index":86},{"id":2,"Name":"Nicole","Program":"BASc(FinTech)","Local":"TRUE","Position":1,"Beautiful Index":1000},{"id":3,"Name":"Cherry","Program":"BBA","Local":"False","Position":4,"Beautiful Index":75},{"id":4,"Name":"Katie","Program":"BEng","Local":"TRUE","Position":3,"Beautiful Index":80},{"id":5,"Name":"Susan","Program":"BA","Local":"TRUE","Position":5,"Beautiful Index":73}]')
         else:
             temp = json.loads('[{"id":1,"Name":"Merry","Program":"BBA","Local":"TRUE","Position":2,"Beautiful Index":86},{"id":2,"Name":"Nicole","Program":"BASc(FinTech)","Local":"TRUE","Position":1,"Beautiful Index":1000},{"id":3,"Name":"Cherry","Program":"BBA","Local":"False","Position":4,"Beautiful Index":75},{"id":4,"Name":"Katie","Program":"BEng","Local":"TRUE","Position":3,"Beautiful Index":80},{"id":5,"Name":"Susan","Program":"BA","Local":"TRUE","Position":5,"Beautiful Index":73}]')
+            found = False
             for i in temp:
                 if(i["id"] == data.get("id")):
                     rdata = i
-            else:
+                    found = True
+            if(not found):
                 rdata["error"] = "Participant not found"
     else:
         rdata["error"] = "Wrong action"
