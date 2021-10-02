f3638933df00a435a4b4bf2d5c906330ff326d63
Lee Sze Choi <iamrainlee@gmail.com>
2021-10-02 14:56:56 +0800

    a

--- codeitsuisse/routes/testget.py
+++ codeitsuisse/routes/testget.py
@@ -11,10 +11,13 @@ logger = logging.getLogger(__name__)
 
 @app.route('/testget', methods=['GET'])
 def testget():
-    data = request.args
     rdata = {}
-    if(data["action"] == "get_students"):
-        rdata = json.loads('[{"Name":"Nicole","UID":"3035785611","DOB":"2002-11-08","Local":"TRUE","Beautiful Index":1000},{"Name":"Rain","UID":"3035779571","DOB":"2002-04-05","Local":"TRUE","Beautiful Index":"NULL"}]')
+    if(len(request.args)>0):
+        data = request.args
+        if(data["action"] == "get_students"):
+            rdata = json.loads('[{"Name":"Nicole","UID":"3035785611","DOB":"2002-11-08","Local":"TRUE","Beautiful Index":1000},{"Name":"Rain","UID":"3035779571","DOB":"2002-04-05","Local":"TRUE","Beautiful Index":"NULL"}]')
+        else:
+            rdata["error"] = "Wrong action"
     else:
-        rdata["error"] = "Missing action"
+        rdata["error"] = "Wrong action"
     return json.dumps(rdata)
