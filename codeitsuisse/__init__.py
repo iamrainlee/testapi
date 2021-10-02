8ae2c8692c7b103d5929b2eb173c40c8b1407c5d
Lee Sze Choi <iamrainlee@gmail.com>
2021-10-02 16:25:06 +0800

    a

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,3 +1,4 @@
 from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.testget
+import codeitsuisse.routes.testpost
---
+++ codeitsuisse/routes/testpost.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import logging
+import json
+
+from flask import request, jsonify, Response
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/testpost', methods=['POST'])
+def testpost():
+    try:
+        data = request.get_json()
+    except:
+        return jsonify({'error':'Invalid JSON'})
+    rdata = {}
+    if(data.get('action') is None):
+        rdata["error"] = "Missing action"
+    elif(data["action"] == "get_Ms_HKU"):
+        rdata = json.loads('{"id":2,"Name":"Nicole","Program":"BASc(FinTech)","Local":"TRUE","Position":1}')
+    elif(data["action"] == "get_Ms_HKU_participants"):
+        if(data.get("id") is None):
+            rdata = json.loads('[{"id":1,"Name":"Merry","Program":"BBA","Local":"TRUE","Position":2,"Beautiful Index":86},{"id":2,"Name":"Nicole","Program":"BASc(FinTech)","Local":"TRUE","Position":1,"Beautiful Index":1000},{"id":3,"Name":"Cherry","Program":"BBA","Local":"False","Position":4,"Beautiful Index":75},{"id":4,"Name":"Katie","Program":"BEng","Local":"TRUE","Position":3,"Beautiful Index":80},{"id":5,"Name":"Susan","Program":"BA","Local":"TRUE","Position":5,"Beautiful Index":73}]')
+        else:
+            temp = json.loads('[{"id":1,"Name":"Merry","Program":"BBA","Local":"TRUE","Position":2,"Beautiful Index":86},{"id":2,"Name":"Nicole","Program":"BASc(FinTech)","Local":"TRUE","Position":1,"Beautiful Index":1000},{"id":3,"Name":"Cherry","Program":"BBA","Local":"False","Position":4,"Beautiful Index":75},{"id":4,"Name":"Katie","Program":"BEng","Local":"TRUE","Position":3,"Beautiful Index":80},{"id":5,"Name":"Susan","Program":"BA","Local":"TRUE","Position":5,"Beautiful Index":73}]')
+            for i in temp:
+                if(i["id"] == data.get("id")):
+                    rdata = i
+            else:
+                rdata["error"] = "Participant not found"
+    else:
+        rdata["error"] = "Wrong action"
+    return jsonify(rdata)
