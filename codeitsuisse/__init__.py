7b079fe0cc1f4c4d31b5db2ab4489d4e92fc074e
Lee Sze Choi <iamrainlee@gmail.com>
2021-12-04 21:39:40 +0800

    a

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-mastermind_records = {}
+app.mastermind_records = {}
 import codeitsuisse.routes.testget
 import codeitsuisse.routes.testpost
 import codeitsuisse.routes.exercise1
